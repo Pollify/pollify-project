3e845887c23047a8f1a5482c1e0b081189a54b88
Mike Kotte <mike@mikekotte.nl>
2021-05-27 20:47:51 +0200

    Fixed deployment to GCloud

--- services/gateway-service/src/poll/poll.module.ts
+++ services/gateway-service/src/poll/poll.module.ts
@@ -1,25 +1,35 @@
 import { Module } from '@nestjs/common';
 import { PollService } from './poll.service';
 import { PollController } from './poll.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { POLL_PACKAGE_NAME } from 'src/generated/protos/polls/polls';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [PollController],
-  providers: [PollService],
-  imports: [
-    ClientsModule.register([
-      {
-        name: POLL_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          url: '172.17.0.17:3000',
-          package: POLL_PACKAGE_NAME,
-          protoPath: join(__dirname, '/../generated/protos/polls/polls.proto'),
-        },
+  providers: [
+    {
+      provide: POLL_PACKAGE_NAME,
+      useFactory: (configService: ConfigService) => {
+        return ClientProxyFactory.create({
+          transport: Transport.GRPC,
+          options: {
+            url: `${configService.get('POLL_SVC')}:${configService.get(
+              'GRPC_PORT',
+            )}`,
+            package: POLL_PACKAGE_NAME,
+            protoPath: join(
+              __dirname,
+              '/../generated/protos/polls/polls.proto',
+            ),
+          },
+        });
       },
-    ]),
+      inject: [ConfigService],
+    },
+    PollService,
   ],
+  imports: [ConfigModule],
 })
 export class PollModule {}
